26e709f28212e539ed1af44bd29c626822701708
Git1503 <154297733+Git1503@users.noreply.github.com>
2025-09-23 15:48:37 +0530

    Create val_only read variable.kt

---
+++ val_only read variable.kt
@@ -0,0 +1,18 @@
+fun main() 
+{
+ val myString = "KOTLIN"
+ println(myString[0])
+ println(myString[1])
+ println(myString.isEmpty())
+ println(myString.length)
+ println(myString.substring(2, 4))
+ println("The string is $myString")
+}
+
+//output
+K
+O
+false
+6
+TL
+The string is KOTLIN
